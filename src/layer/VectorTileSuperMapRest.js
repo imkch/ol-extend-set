40c523a960142dcdf3a5e0747de93351de1c9197
kunchen <kunchen@imkch.com>
2021-01-14 17:25:10 +0800

    提取矢量切片公共style加载方法

--- src/layer/VectorTileSuperMapRest.js
+++ src/layer/VectorTileSuperMapRest.js
@@ -1,5 +1,5 @@
 import VectorTile from 'ol/layer/VectorTile';
-import stylefunction from 'ol-mapbox-style/src/stylefunction';
+import { loadVectorTileStyle } from '../style/VectorTile';
 
 export default class VectorTileArcGISRest extends VectorTile {
   constructor(options) {
@@ -10,55 +10,11 @@ export default class VectorTileArcGISRest extends VectorTile {
     this.source = options.source;
     this.credentials = options.withCredentials ? 'include' : 'omit';
     this.headers = options.headers || {};
-    this._styleLoadFunction();
+    this.styleLoadFunction();
   }
-  _styleLoadFunction() {
-    const { source, credentials, headers } = this;
-    const styleUrl = `${source.baseUrl}/style.json`;
-    fetch(styleUrl, { headers, credentials })
-      .then(response => response.json())
-      .then(data => {
-        this._loadStyle(data);
-      })
-      .catch(e => {
-        console.error('load style error', e);
-      })
-  }
-  _loadStyle(styleJson) {
-    const { source, credentials, headers } = this;
-    const spriteScale = window.devicePixelRatio >= 1.5 ? 0.5 : 1;
-    const sizeFactor = spriteScale === 0.5 ? '@2x' : '';
-    const sourceName = Object.keys(styleJson.sources)[0];
-    const tileGrid = source.getTileGrid();
-    const resolutions = tileGrid.getResolutions();
-    const spriteImageUrl = styleJson.sprite + sizeFactor + '.png';
-    const style = this._getStyle(styleJson, sourceName, resolutions, null, spriteImageUrl);
-    this.setStyle(style);
-    
-    const spriteUrl = styleJson.sprite + sizeFactor + '.json';
-    fetch(spriteUrl, { headers, credentials })
-      .then(response => response.json())
-      .then(data => {
-        const style = this._getStyle(styleJson, sourceName, resolutions, data, spriteImageUrl);
-        this.setStyle(style);
-        this.setVisible(this.visible);
-      })
-      .catch(e => {
-        console.error('load sprite error', e);
-      })
-  }
-  _getStyle(styleJson, sourceName, resolutions, sprite, spriteImageUrl) {
-    const fun = stylefunction(
-      this,
-      styleJson,
-      sourceName,
-      resolutions,
-      sprite,
-      spriteImageUrl,
-      () => []
-    );
-    return (feature, resolution) => {
-      return fun(feature, resolution);
-    };
+  styleLoadFunction() {
+    const styleUrl = this.source.baseUrl + '/style.json';
+    const spriteUrl = this.source.baseUrl + '/sprites/sprite';
+    loadVectorTileStyle(this, styleUrl, spriteUrl);
   }
 }
---
+++ src/style/VectorTile.js
@@ -0,0 +1,53 @@
+import stylefunction from 'ol-mapbox-style/src/stylefunction';
+
+function loadStyle(styleJson, vectorTileLayer, spriteUrl) {
+  const { source, visible, credentials, headers } = vectorTileLayer;
+  const spriteScale = window.devicePixelRatio >= 1.5 ? 0.5 : 1;
+  const sizeFactor = spriteScale === 0.5 ? '@2x' : '';
+  const sourceName = Object.keys(styleJson.sources)[0];
+  const tileGrid = source.getTileGrid();
+  const resolutions = tileGrid.getResolutions();
+  const spriteImageUrl = spriteUrl + sizeFactor + '.png';
+  const style = getStyle(vectorTileLayer, styleJson, sourceName, resolutions, null, spriteImageUrl);
+  vectorTileLayer.setStyle(style);
+  
+  const spriteJsonUrl = spriteUrl + sizeFactor + '.json';
+  fetch(spriteJsonUrl, { headers, credentials })
+    .then(response => response.json())
+    .then(data => {
+      const style = getStyle(vectorTileLayer, styleJson, sourceName, resolutions, data, spriteImageUrl);
+      vectorTileLayer.setStyle(style);
+      vectorTileLayer.setVisible(visible);
+    })
+    .catch(e => {
+      console.error('load sprite error', e);
+    })
+}
+function getStyle(vectorTileLayer, styleJson, sourceName, resolutions, sprite, spriteImageUrl) {
+  const fun = stylefunction(
+    vectorTileLayer,
+    styleJson,
+    sourceName,
+    resolutions,
+    sprite,
+    spriteImageUrl,
+    () => []
+  );
+  return (feature, resolution) => {
+    return fun(feature, resolution);
+  };
+}
+
+const loadVectorTileStyle = (vectorTileLayer, styleUrl, spriteUrl) => {
+  const { credentials, headers } = vectorTileLayer;
+  fetch(styleUrl, { headers, credentials })
+    .then(response => response.json())
+    .then(data => {
+      loadStyle(data, vectorTileLayer, spriteUrl);
+    })
+    .catch(e => {
+      console.error('load style error', e);
+    })
+};
+
+export { loadVectorTileStyle };
